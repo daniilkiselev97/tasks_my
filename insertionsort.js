051998644f1712b8b0aff4382a86771f8429d430
Daniil Kiselev <danqkiselev@yandex.ru>
2023-08-21 20:08:52 +0300

    insertionSort done

---
+++ insertionsort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+    let len = arr.length;
+    for (let i = 1; i < len; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j > -1 && current < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+console.log(insertionSort([-5, 0, 12, 1, 87, 3]));
